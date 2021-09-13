83c9f4f39e0d274712dbe3a0d3e0e9f668b3a666
Mikael <dev.kov.mtl@gmail.com>
2021-09-13 16:33:42 -0400

    Added listener to button and added two functions

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,24 @@
 const displayScreen = document.querySelector('.calc-screen')
-const buttons = document.querySelectorAll('button')
-let num = ''
-let displayNum = []
+const numericBtns = document.querySelectorAll('button.numeric')
+const operandBtns = document.querySelectorAll('button.operand')
+
+const OPERAND = [
+  'equal',
+  'clear',
+  'divide',
+  'multiply',
+  'cancel',
+  'subtract',
+  'add',
+  'negate',
+  'float',
+]
+
+let userInput = ''
+const data = []
+const operations = []
+let lastData = ''
+
 function add(a, b) {
   return a + b
 }
@@ -29,19 +46,50 @@ function operate(fn, a, b) {
   return fn(a, b)
 }
 
-function display(str) {
+function displayToScreen(str) {
   displayScreen.textContent = str
 }
 
-function onBtnClick(e) {
-  const name = e.target.name
-
-  displayScreen.textContent = displayNum
-  if (name === 'equal') {
-    console.log('Equal')
-  }
+function onNumericBtnClick(e) {
+  userInput += e.target.name
+  displayToScreen(userInput)
 }
 
-buttons.forEach((button) => {
-  button.addEventListener('click', onBtnClick)
+function onOperandBtnClick(e) {
+  const operandName = e.target.name
+  const operandSymbol = e.target.innerHTML
+  // console.log(e.target.name)
+  // console.log(e.target.innerHTML)
+
+  console.log(userInput)
+  data.push(userInput)
+  data.push(operandSymbol)
+  operations.push(operandName)
+  userInput = userInput + operandSymbol
+  displayToScreen(userInput)
+}
+
+function computeIntermediateStep() {
+  console.log(data)
+}
+
+numericBtns.forEach((numericBtn) => {
+  numericBtn.addEventListener('click', (e) => {
+    lastData = data[data.length - 1]
+    console.log(lastData)
+    onNumericBtnClick(e)
+  })
+})
+
+operandBtns.forEach((operandBtn) => {
+  operandBtn.addEventListener('click', (e) => {
+    lastData = data[data.length - 1]
+
+    if (operations.length === 0) {
+      onOperandBtnClick(e)
+    } else {
+      // compute step
+      computeIntermediateStep()
+    }
+  })
 })
